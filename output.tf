42d2fd28a52469405437fbd9d93adb7f0845fd80
Alban MUSSI <alban_mussi@uptimemanagers.fr>
2020-09-30 21:50:44 +0200

    fix: output values

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.vpc.vpc_id.value
 }
 
 output "azs" {
@@ -8,9 +8,9 @@ output "azs" {
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  value = module.vpc.private_subnets.value
 }
 
-output "public_subnets" {
-  value = module.vpc.public_subnets
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets.value
 }
